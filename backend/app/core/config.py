1a518889a66767a9a7a43edea05fe8c33fcad168
Eduarda <eduardamartins4141@gmail.com>
2026-06-11 22:20:51 -0300

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    REDIS_URL: str = ""
+    REDIS_URL: Optional[str] = None
+
     SECRET_KEY: str = "supersecretkey"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
